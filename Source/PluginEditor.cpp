aa8a65fcd79193c8d94d1a0b1bbe8aa06695da7b
maximosan <maximosantoro@protonmail.com>
2021-05-24 19:54:27 -0300

    seteado - a aprender a correr esta pija

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -32,7 +32,7 @@ void EqoneAudioProcessorEditor::paint (Graphics& g)
 
     g.setColour (Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    g.drawFittedText ("la concha de tu madre", getLocalBounds(), Justification::centred, 1);
 }
 
 void EqoneAudioProcessorEditor::resized()
